b54d0f0287055d2f57b985d1e8e8c32bb3583f77
Davideee03 <daaavi03@gmail.com>
2024-10-17 14:02:10 +0200

    Enemy update
    Now the enemies targets the player and the latter has a health system.

--- Scripts/Enemies/EnemyMovement.gd
+++ Scripts/Enemies/EnemyMovement.gd
@@ -7,22 +7,36 @@ var can_attack : bool = false
 
 @export_category("Damage")
 @export var damage_inflicted : float = 25.0
-@export var attack_time : float = 2.5
+@export var attack_wait_time : float = 0.5
 
 
 func _process(delta):
+	#Move towards the player (=target) only if
+	#it's not attacking
 	if can_attack: return
 	global_position = global_position.move_toward(target.global_position, speed * delta)
 
+#Set what's the target
 func set_target(new_target):
 	target = new_target
 
+#The player is close enough
+#so attack him
 func _on_area_entered(area):
 	attack(area)
-
-func attack(building : Area2D):
 	can_attack = true
-	await get_tree().create_timer(attack_time).timeout
+#If the player isn't close
+#move towards him
+func _on_area_exited(_area):
+	can_attack = false
+
+func attack(player):
+	#Wait until it can attack
+	await get_tree().create_timer(attack_wait_time).timeout
 	
-	building.take_damage(damage_inflicted)
-	attack(building)
+	#If it can't attack, stop the iteration
+	if !can_attack: return
+	
+	player.take_damage(damage_inflicted) #Attack the player
+	attack(player) #Repeat the attack
+
--- Scripts/Player/HealthSystem.gd
+++ Scripts/Player/HealthSystem.gd
@@ -1,9 +1,9 @@
-extends Node
+extends Area2D
 
 @export_category("Health")
-@export var total_health : float = 500.0
+@export var total_health : float = 100.0
 var current_health : float
-@export var can_take_damage : bool = false
+@export var can_take_damage : bool = true
 
 @export_category("Invincibility")
 @export var invincibility_time : float = 0.25
@@ -11,9 +11,11 @@ var current_health : float
 func _ready():
 	current_health = total_health
 
-func _on_hitbox_area_entered(area):
-	if !can_take_damage: return
-	current_health-=area.damage
+func take_damage(damage : float):
+	if !can_take_damage: return #If can't take damage return
+	current_health-=damage #Reduce the damage_taken
 	
+	#Reload the scene if he's dead
+	####MOMENTANEO####
 	if current_health<=0:
 		get_tree().reload_current_scene()
