13b65060e2580ab134b459a3851a14da98c358ff
Yuuki <yuukigpatriarca@gmail.com>
2024-10-19 23:11:13 +0200

    readjusting the shop

--- Scripts/UI/shop.gd
+++ Scripts/UI/shop.gd
@@ -2,5 +2,8 @@ extends TabContainer
 
 
 func _ready():
+	# getting the display size of the mobile
 	var screen_size = get_viewport_rect().size
+	# setting the minimum size of the shop to 80% of the screen
+	# for future implementation we can also create a function and make some staples for like tablet or other idk
 	custom_minimum_size = screen_size * 0.8  
