9f75569b61ebed67e1d221f9cd482a71c1a8a864
Davideee03 <daaavi03@gmail.com>
2024-11-04 13:21:38 +0100

    Little ui adjustments

--- Scripts/UI/ShopButton.gd
+++ Scripts/UI/ShopButton.gd
@@ -1,16 +1,15 @@
 extends Control
 
-@export_category("Animation")
-@export var animation_speed : float = 5.0
-@onready var animation_player = %AnimationPlayer
+@onready var buttons: Control = %Buttons
 
 func _on_button_down():
 	#If the "Shop" button is pressed show the shop
-	animation_player.play("Shop", -1, animation_speed)
+	buttons.visible = false
+	visible = true
 	Global.shop_opened = true
 
 func _on_exit_button_button_down():
 	#Close the shop
-	animation_player.play("Shop", -1, -animation_speed, true)
-	#visible = false
+	buttons.visible = true
+	visible = false
 	Global.shop_opened = false
