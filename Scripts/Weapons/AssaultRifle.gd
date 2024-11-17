5f5ba0b00534d35a2e38c7de4be3cdca789975c4
FConsalvi <188714287+FConsalvi@users.noreply.github.com>
2024-11-17 20:05:24 +0100

    Weapons Update

--- Scripts/Weapons/AssaultRifle.gd
+++ Scripts/Weapons/AssaultRifle.gd
@@ -1,7 +1,7 @@
 class_name AssaultRifle
 extends Weapon
 
-var sprite = preload("res://Sprites/Weapons/Gun.png")
-var bullet = preload("res://Scenes/Weapon/Bullets/GunBullet.tscn")
+var sprite = preload("res://Sprites/Weapons/AssaultRifle.png")
+var bullet = preload("res://Scenes/Weapon/Bullets/AssaultRifleBullet.tscn")
 var damage = 15
 var reload_time = 0.3
