8c1d43a9e4c72ef85963367469af6a5a7367fd69
FConsalvi <188714287+FConsalvi@users.noreply.github.com>
2024-12-15 13:48:26 +0100

    Work in Progress on Mine

---
+++ Scripts/UI/ExplosivesButton.gd
@@ -0,0 +1,15 @@
+class_name ExplosivesShopButton
+extends Button
+
+var explosives : Node2D
+
+@export var new_explosives : Explosives
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
