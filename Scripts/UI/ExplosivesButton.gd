1a55e8309ca32df0578ae0a7e82b673ca9c7e91d
FConsalvi <188714287+FConsalvi@users.noreply.github.com>
2024-12-30 13:37:58 +0100

    Work in Progress on Mine

--- Scripts/UI/ExplosivesButton.gd
+++ Scripts/UI/ExplosivesButton.gd
@@ -1,10 +1,10 @@
-class_name ExplosivesShopButton
+class_name ExplosivesButton
 extends Control
 
 @export var mine: PackedScene
 var mine_count : float = 0
-var mine_button : Button
-var player : CharacterBody2D
+@onready var mine_button : Button = $Mine
+@onready var player : CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,7 +14,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
+#Creates the mine at the player's position
 func _on_mine_button_down() -> void:
 	player = get_parent().get_parent().get_parent()
 	await get_tree().create_timer(2).timeout
--- Scripts/UI/ExplosivesShop.gd
+++ Scripts/UI/ExplosivesShop.gd
@@ -1,11 +1,11 @@
-extends ScrollContainer
-
-var mine_count : float = 0
+extends ExplosivesButton
 
 
 func _ready():
 	pass
 
-
+#Update purchased mine counter
 func _on_mine_pressed() -> void:
 	mine_count += 1
+	mine_button.text = str(mine_count)
+	print(mine_count)
