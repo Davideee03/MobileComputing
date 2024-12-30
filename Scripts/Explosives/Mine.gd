6a2f0cdfb64c1bc1b463b139f65405fdc3310cb1
FConsalvi <188714287+FConsalvi@users.noreply.github.com>
2024-12-30 01:14:24 +0100

    Work in Progress on Mine

--- Scripts/Explosives/Mine.gd
+++ Scripts/Explosives/Mine.gd
@@ -14,11 +14,15 @@ func _process(delta: float) -> void:
 
 # Check if enemies have entered the area
 func _on_detector_area_entered(area):
+	await get_tree().create_timer(time).timeout
 	area.take_damage(damage)
 	print("Danno")
 	queue_free()
 
 func _on_button_down() -> void:
+	var player = get_node("Player")
+	print("Ok")
+	print(player.global_position)
 	var explosive = mine.instantiate()
 	add_child(explosive)
-	explosive.global_position = position
+	explosive.global_position = player.global_position
--- Scripts/UI/ExplosivesButton.gd
+++ Scripts/UI/ExplosivesButton.gd
@@ -7,13 +7,13 @@ extends Control
 func _ready() -> void:
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_mine_button_down() -> void:
+	var player = get_parent().get_parent()
+	await get_tree().create_timer(2).timeout
 	var explosive = mine.instantiate()
 	get_tree().root.add_child(explosive)
-	explosive.global_position = Vector2(200,200)
+	explosive.global_position = player.global_position
