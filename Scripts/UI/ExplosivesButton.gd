be58f3c3dd6b1de62a51153a91b00aebe55cfdd1
FConsalvi <188714287+FConsalvi@users.noreply.github.com>
2024-12-31 16:24:13 +0100

    Improved Explosives Handling

--- Scripts/UI/ExplosivesButton.gd
+++ Scripts/UI/ExplosivesButton.gd
@@ -1,12 +1,17 @@
 class_name ExplosivesButton
 extends Control
 
+var game_started : bool = false
+
 @export var mine: PackedScene
 @export var grenade: PackedScene
+
 var mine_count : float = 1
 var grenade_count : float = 1
+
 @onready var mine_button : Button = $Mine
 @onready var grenade_button : Button = $Grenade
+
 var player : CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
@@ -17,10 +22,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 	
+func _on_start_wave_button_button_down() -> void:
+	game_started = true
+	
 
 #Creates the mine at the player's position
 func _on_mine_button_down() -> void:
-	if mine_count > 0:
+	if (mine_count > 0) and game_started:
 		player = get_parent().get_parent().get_parent()
 		await get_tree().create_timer(2).timeout
 		var explosive = mine.instantiate()
@@ -29,12 +37,14 @@ func _on_mine_button_down() -> void:
 		mine_count -= 1
 		print("Mines available: "+ str(mine_count))
 		mine_button.text = "Mine: " + str(mine_count)
+	elif (mine_count == 0):
+		print("No mines available")
 	else:
-		print ("No mines available")
+		print("Game not started yet")
 
 #Creates the Grenade at the player's position
 func _on_grenade_button_down() -> void:
-	if grenade_count > 0:
+	if (grenade_count > 0) and game_started:
 		player = get_parent().get_parent().get_parent()
 		await get_tree().create_timer(2).timeout
 		var explosive = grenade.instantiate()
@@ -43,5 +53,7 @@ func _on_grenade_button_down() -> void:
 		grenade_count -= 1
 		print("Grenades available: "+ str(grenade_count))
 		grenade_button.text = "Grenade: " + str(grenade_count)
+	elif (mine_count == 0):
+		print("No Grenades available")
 	else:
-		print ("No Grenades available")
+		print("Game not started yet")
