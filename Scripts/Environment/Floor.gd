376c46b71f6d22543713bddc45c950da10cd7123
Davideee03 <daaavi03@gmail.com>
2025-02-02 10:42:41 +0100

    Floors spawning system optimization

--- Scripts/Environment/Floor.gd
+++ Scripts/Environment/Floor.gd
@@ -1,59 +1,18 @@
-extends Sprite2D
+extends Node2D
 
-#Reference to the floor
-const FLOOR = preload("res://Scenes/Environment/Floor.tscn")
+@onready var spawn_positions: Node2D = $SpawnPositions
+@onready var destroy_area: Area2D = $Destroy
 
-#Reference to the parent
-#It stores the floors position
-@onready var floor: Node = $".."
+@export var is_first_floor : bool = false
 
-@export var first_tile : bool = false
+func _on_player_detector_body_entered(_body: Node2D) -> void:
+	for marker in spawn_positions.get_children():
+		get_parent().spawn(marker.global_position)
 
-#Add to the parent it's position
-func _ready() -> void:
-	floor.add(global_position)
+func _on_destroy_body_exited(_body: Node2D) -> void:
+	delete_floor()
 
-func spawn():
-	#It's the y position multiplier
-	var y : int = -1
-	#It's the x position multiplier
-	var x : int = -1
-	#A counter
-	var j : int = 0
-	
-	while y<2:
-		#If there's no floor in this position, spawn 
-		if !floor.has(global_position+Vector2(x*texture.get_width(), y*texture.get_height())):
-			var next_floor = FLOOR.instantiate()
-			get_parent().call_deferred("add_child", next_floor)
-			next_floor.global_position = global_position+Vector2(x*texture.get_width(), y*texture.get_height())
-		if !floor.has(global_position+Vector2(0, texture.get_height())):
-			var next_floor = FLOOR.instantiate()
-			get_parent().call_deferred("add_child", next_floor)
-			next_floor.global_position = global_position+Vector2(0, texture.get_height())
-		if !floor.has(global_position+Vector2(0, -texture.get_height())):
-			var next_floor = FLOOR.instantiate()
-			get_parent().call_deferred("add_child", next_floor)
-			next_floor.global_position = global_position+Vector2(0, -texture.get_height())
-		
-		#Increase the counter and the multipliers
-		if j<1:
-			j+=1
-		else:
-			j = 0
-			y+=1
-		x*=-1
-
-#Remove the node from the tree and from the floor_container
-func delete():
-	if first_tile: return
-	floor.delete(global_position)
+func delete_floor():
+	if is_first_floor: return
+	get_parent().delete_floor(global_position)
 	queue_free()
-
-#Spawn new floors when the player enters in the floor area
-func _on_player_detector_body_entered(body: Node2D) -> void:
-	spawn()
-
-#Remove the floor when it's not vible
-func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
-	delete()
