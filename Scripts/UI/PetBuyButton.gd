cffbcdf38d3c076f3f4dae8b398e1f10bbbed9a7
Davideee03 <daaavi03@gmail.com>
2025-02-19 12:42:07 +0100

    Last update
    Pet buy button didn't reduce the Legendary cores

--- Scripts/UI/PetBuyButton.gd
+++ Scripts/UI/PetBuyButton.gd
@@ -1,14 +1,16 @@
 extends Button
 
 @onready var label_node = get_node("/root/World/Pet/Weapon")
+const price : int = 20
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if SaveConsumables.consume_stats["Pet"]["bought"]==true:
 		hide()
 
 func _on_button_down() -> void:
-	if Stats.current_coreLegendary >= 20:
+	if Stats.current_coreLegendary >= price:
 		SaveConsumables.consume_stats["Pet"]["bought"] = true
+		Stats.current_coreLegendary -= price
 		label_node.enable_pet()
 		hide()
 		Stats.emit_cores()
